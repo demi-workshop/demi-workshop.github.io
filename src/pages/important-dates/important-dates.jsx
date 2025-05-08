d4ed31eda116e6dbaaf37199a5f149c016a0ae07
niyoj <oliniyoj@gmail.com>
2025-05-09 00:01:11 +0545

    adds important dates

--- src/pages/important-dates/important-dates.jsx
+++ src/pages/important-dates/important-dates.jsx
@@ -1,3 +1,13 @@
+import styles from "./style.module.css";
+
+const dates = [
+  { title: "Paper Submission Begins", date: "12 May, 2025" },
+  { title: "Submission Deadline", date: "20 June, 2025" },
+  { title: "Paper Decision Notification", date: "15 July, 2025" },
+  { title: "Camera Ready Submission", date: "21 July, 2025" },
+  { title: "Workshop Day", date: "27 September, 2025" },
+];
+
 export default function ImportantDatesPage() {
   return (
     <section id="important-dates">
@@ -9,9 +19,23 @@ export default function ImportantDatesPage() {
           fontSize: "2rem",
           textAlign: "center",
         }}
-      >
-        Coming Soon...
-      </p>
+      ></p>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <th>Event</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {dates.map((item, index) => (
+            <tr key={index}>
+              <td>{item.title}</td>
+              <td>{item.date}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </section>
   );
 }
