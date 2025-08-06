afa6b90b3fc50e77b2c0788d8037e235461ee92b
niyoj <oliniyoj@gmail.com>
2025-08-06 19:48:37 +0545

    adds call for papers

--- src/pages/call-for-papers/call-for-papers.jsx
+++ src/pages/call-for-papers/call-for-papers.jsx
@@ -1,76 +1,118 @@
-import { Link } from "react-router-dom";
+import { useEffect } from "react";
+import { useLocation, Link } from "react-router-dom";
+import acceptedPapers from "../../data/accepted-papers.json";
 
 export default function CallForPapersPage() {
-  return (
-    <section id="call-for-papers">
-      <h2>Call for Papers</h2>
-      <p>
-        We invite researchers, practitioners, and enthusiasts to submit original
-        contributions aligned with the themes outlined on our&nbsp;
-        <Link to="/workshop-details">workshop details page</Link>. Please review
-        the submission guidelines and ensure your work adheres to the specified
-        format and deadlines listed on the&nbsp;
-        <Link to="/important-dates">important dates page</Link>. Join us in
-        advancing discussions and innovation in this exciting area!
-      </p>
-      <h3>How to Submit?</h3>
-      <p>
-        <strong className="u-color-secondary" style={{ fontWeight: 600 }}>
-          CMT submission website:
-        </strong>
-        &nbsp;
-        <a href="https://cmt3.research.microsoft.com/DEMI2025/">CMT DEMI</a>
-      </p>
-      <p style={{ marginBottom: 0 }}>
-        We welcome your valuable contributions. Please follow the steps below to
-        begin the submission process:
-      </p>
-      <ul>
-        <li style={{ marginBottom: 0 }}>
-          <a href="https://cmt3.research.microsoft.com/docs/help/general/account-creation.html">
-            Create an author account
-          </a>
-          <span>
-            &nbsp;to manage your submission and receive important updates.
-          </span>
-        </li>
-        <li>
-          <a href="https://cmt3.research.microsoft.com/docs/help/author/author-submission-form.html">
-            How to submit a paper
-          </a>
-          <span>
-            , a step-by-step guide to help you prepare and submit your
-            manuscript.
-          </span>
-        </li>
-      </ul>
-      <p>
-        Accepted papers will be published in a joint proceeding with the MICCAI
-        2024 conference.
-      </p>
-      <p>
-        All papers should be formatted according to the &nbsp;
-        <a href="https://www.springer.com/gp/computer-science/lncs/conference-proceedings-guidelines">
-          Lecture Notes in Computer Science templates
-        </a>
-        &nbsp;. We recommend submission up to 8-pages and 2-pages of references
-        (same as MICCAI main conference) for a double-blind peer review process
-        In addition, since the joint workshop has adhered to the double-blinded
-        peer review process, we ask that you please follow the &nbsp;
-        <a href="https://conferences.miccai.org/2024/en/PAPER-SUBMISSION-AND-REBUTTAL-GUIDELINES.html">
-          MICCAI2024 anonymity guidelines
-        </a>
-        &nbsp; when preparing your initial submission.
-      </p>
-      <h3>CMT Acknowledgement</h3>
-      <p>
-        The &nbsp;
-        <a href="https://cmt3.research.microsoft.com/">Microsoft CMT service</a>
-        &nbsp;was used for managing the peer-reviewing process for this
-        conference. This service was provided for free by Microsoft and they
-        bore all expenses, including costs for Azure cloud services as well as
-        for software development and support.
-      </p>
-    </section>
-  );
+	const location = useLocation();
+
+	useEffect(() => {
+		const hashIndex = location.hash.indexOf("#");
+		if (hashIndex !== -1) {
+			const elementId = location.hash.substring(hashIndex + 1);
+			const el = document.getElementById(elementId);
+			if (el) {
+				el.scrollIntoView({ behavior: "smooth" });
+			}
+		}
+	}, [location]);
+
+	return (
+		<>
+			<section id="call-for-papers">
+				<h2>Call for Papers</h2>
+				<p>
+					We invite researchers, practitioners, and enthusiasts to submit
+					original contributions aligned with the themes outlined on our&nbsp;
+					<Link to="/workshop-details">workshop details page</Link>. Please
+					review the submission guidelines and ensure your work adheres to the
+					specified format and deadlines listed on the&nbsp;
+					<Link to="/important-dates">important dates page</Link>. Join us in
+					advancing discussions and innovation in this exciting area!
+				</p>
+				<h3>How to Submit?</h3>
+				<p>
+					<strong className="u-color-secondary" style={{ fontWeight: 600 }}>
+						CMT submission website:
+					</strong>
+					&nbsp;
+					<a href="https://cmt3.research.microsoft.com/DEMI2025/">CMT DEMI</a>
+				</p>
+				<p style={{ marginBottom: 0 }}>
+					We welcome your valuable contributions. Please follow the steps below
+					to begin the submission process:
+				</p>
+				<ul>
+					<li style={{ marginBottom: 0 }}>
+						<a href="https://cmt3.research.microsoft.com/docs/help/general/account-creation.html">
+							Create an author account
+						</a>
+						<span>
+							&nbsp;to manage your submission and receive important updates.
+						</span>
+					</li>
+					<li>
+						<a href="https://cmt3.research.microsoft.com/docs/help/author/author-submission-form.html">
+							How to submit a paper
+						</a>
+						<span>
+							, a step-by-step guide to help you prepare and submit your
+							manuscript.
+						</span>
+					</li>
+				</ul>
+				<p>
+					Accepted papers will be published in a joint proceeding with the
+					MICCAI 2024 conference.
+				</p>
+				<p>
+					All papers should be formatted according to the &nbsp;
+					<a href="https://www.springer.com/gp/computer-science/lncs/conference-proceedings-guidelines">
+						Lecture Notes in Computer Science templates
+					</a>
+					&nbsp;. We recommend submission up to 8-pages and 2-pages of
+					references (same as MICCAI main conference) for a double-blind peer
+					review process In addition, since the joint workshop has adhered to
+					the double-blinded peer review process, we ask that you please follow
+					the &nbsp;
+					<a href="https://conferences.miccai.org/2024/en/PAPER-SUBMISSION-AND-REBUTTAL-GUIDELINES.html">
+						MICCAI2024 anonymity guidelines
+					</a>
+					&nbsp; when preparing your initial submission.
+				</p>
+				<h3>CMT Acknowledgement</h3>
+				<p>
+					The &nbsp;
+					<a href="https://cmt3.research.microsoft.com/">
+						Microsoft CMT service
+					</a>
+					&nbsp;was used for managing the peer-reviewing process for this
+					conference. This service was provided for free by Microsoft and they
+					bore all expenses, including costs for Azure cloud services as well as
+					for software development and support.
+				</p>
+			</section>
+
+			<section id="accepted-papers">
+				<h2>Accepted Papers</h2>
+				<ul>
+					{Object.keys(acceptedPapers).map((paper) => (
+						<li key={paper}>
+							<span>
+								<p
+									style={{
+										fontWeight: 700,
+										marginBottom: 0,
+										color: "var(--color-secondary)",
+									}}
+								>
+									{paper}
+								</p>
+								<p style={{ marginTop: 0 }}>{acceptedPapers[paper]}</p>
+							</span>
+						</li>
+					))}
+				</ul>
+			</section>
+		</>
+	);
 }
